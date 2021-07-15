35dccd2e3b5162530871b9d444f6555e795cafbc
Akshitha-04 <71962831+Akshitha-04@users.noreply.github.com>
2021-07-15 20:12:15 +0530

    first commit

--- 04-circlesintersect-Python/circlesintersect.py
+++ 04-circlesintersect-Python/circlesintersect.py
@@ -6,9 +6,9 @@
 
 def fun_circlesintersect(x1, y1, r1, x2, y2, r2):
 	# your code goes here
-	dis = (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)
-	rad = (r1 + r2) * (r1 + r2);
-	if dis==rad or dis<rad:
+	distance = (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)
+	radius = (r1 + r2) * (r1 + r2);
+	if distance == radius or distance < radius:
 		return True
 	else:
 		return False
