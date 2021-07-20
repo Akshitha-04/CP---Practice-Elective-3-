eb1c65ef8a4df2eec7926f586725d6971c58e645
Akshitha-04 <71962831+Akshitha-04@users.noreply.github.com>
2021-07-20 17:43:39 +0530

    commit

--- 05-alternatingsum-Python/alternatingsum.py
+++ 05-alternatingsum-Python/alternatingsum.py
@@ -3,9 +3,24 @@
 # For example, alternatingSum([5,3,8,4]) returns 6 (that is, 5-3+8-4). If the list is empty, return 0.
 
 
-
+# def readArray():
+# 	a = []
+# 	l = int(input())
+# 	for i in range(l):
+# 		a.append(int(input()))
+# 	return a
 
 def fun_alternatingsum(a): 
-	return 0
+	sum1=0
+	sum2=0
+	if(len(a)==0):
+		return 0
+	for i in range(0,len(a),1):
+		if(i%2 == 0):
+			sum1 += a[i]
+		else:
+			sum2 += a[i]
+	return sum1 - sum2
+
 
 
