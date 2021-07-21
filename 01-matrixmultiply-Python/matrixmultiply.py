31b200a8fc8b79ca735eb2fbab6f43d92318b61d
Akshitha-04 <71962831+Akshitha-04@users.noreply.github.com>
2021-07-21 14:47:40 +0530

    commit

--- 01-matrixmultiply-Python/matrixmultiply.py
+++ 01-matrixmultiply-Python/matrixmultiply.py
@@ -5,8 +5,16 @@
 
 
 def fun_matrixmultiply(m1, m2):
-    return None
-
-
-
-
+    
+    if(len(m1[0]))!=len(m2):
+        return None
+    else:
+        m3 = []
+        for i in range(len(m1)):
+            m3 +=[[0] * len(m2[0])]
+            
+        for i in range(len(m1)):
+            for j in range(len(m2[0])):
+                for k in range(len(m2)):
+                    m3[i][j] += m1[i][k] * m2[k][j]
+    return m3
