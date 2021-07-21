b2ec15737e6bc003fdaa76ebb778c5bc617ba457
Akshitha-04 <71962831+Akshitha-04@users.noreply.github.com>
2021-07-21 22:34:21 +0530

    commit

--- 07-digitcount-Python/digitcount.py
+++ 07-digitcount-Python/digitcount.py
@@ -6,5 +6,12 @@
 # repeatedly remove the ones digit until you cannot.
 
 def digitcount(n):
-	# your code goes here
-	pass
+    x = abs(n)
+    count = 0
+    if(n == 0):
+        return 1
+    else:
+        while (n!=0):
+            	n//=10
+            	count+=1
+        return count
