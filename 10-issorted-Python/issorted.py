9cfe981b4da7ad6f5600203d43cb48e11dd74180
Akshitha-04 <71962831+Akshitha-04@users.noreply.github.com>
2021-07-27 17:09:14 +0530

    commit

--- 10-issorted-Python/issorted.py
+++ 10-issorted-Python/issorted.py
@@ -5,6 +5,17 @@
 # learn soon, it runs in O(n) time, where n=len(a)), and so in particular you may not sort 
 # the list.
 
+
 def issorted(a):
-	# your code goes here
-	pass
+    # your code goes here
+    count=0
+    for i in range(len(a)):
+        if(i==len(a)-1):
+            break
+        if(a[i]>a[i+1]):
+            count+=1
+    if(count==0):
+        return True
+    elif(count==len(a)-1):
+        return True
+    return False
